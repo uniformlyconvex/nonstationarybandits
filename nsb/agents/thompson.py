1876539101f2e8990c2b6b1c86fae320162923c1
Michael Scoones <michael.scoones.22@ucl.ac.uk>
2024-12-09 09:03:23 +0000

    Fix thompson sampling with clipping

--- nsb/agents/thompson.py
+++ nsb/agents/thompson.py
@@ -8,6 +8,7 @@ from nsb.agents.base import MABAgent, MABAgentParams, MABObservation
 from nsb.environment import MABEnvironment
 from nsb.distributions.conjugacy import PriorPosterior, Likelihood
 
+
 @dataclass(frozen=True)
 class TSParams(MABAgentParams):
     # For now we only accept likelihoods with conjugate priors
@@ -24,37 +25,103 @@ class TSAgent(MABAgent[TSParams]):
         self._store_posteriors()
 
     def __str__(self) -> str:
-        priors_text = str([likelihood.distribution_type for likelihood in self._params.likelihoods])
+        priors_text = str(
+            [likelihood.distribution_type for likelihood in self._params.likelihoods]
+        )
         return f"TSAgent(priors={priors_text})"
-    
+
     def __repr__(self) -> str:
         return "TSAgent"
-    
+
     def _store_posteriors(self) -> None:
-        posteriors = copy.deepcopy([
-            likelihood.prior.distribution
-            for likelihood in self._params.likelihoods
-        ])
+        posteriors = copy.deepcopy(
+            [likelihood.prior.distribution for likelihood in self._params.likelihoods]
+        )
         self._posteriors.append(posteriors)
-    
+
     def observe(self, observation: MABObservation) -> None:
         super().observe(observation)
-        self._params.likelihoods[observation.arm_pulled].update_prior([observation.reward])
+        self._params.likelihoods[observation.arm_pulled].update_prior(
+            [observation.reward]
+        )
         self._store_posteriors()
-
 
     def pick_action(self) -> int:
         param_samples = [
             likelihood.sample_prior_distribution()
             for likelihood in self._params.likelihoods
         ]
+        # torch distributions supoport broadcasting so a single distribution object can be used
         beliefs: list[dist.Distribution] = [
             likelihood.distribution(**params)
             for likelihood, params in zip(self._params.likelihoods, param_samples)
         ]
         means = [belief.mean.item() for belief in beliefs]
         return np.argmax(means)
-    
+
+
+@dataclass(frozen=True)
+class TSClippingParams:
+    likelihoods: tuple[Likelihood]
+    no_samples: int
+    clipping_threshold: float
+
+    def __eq__(self, other) -> bool:
+        return hash(self) == hash(other)
+
+
+class TSClippingAgent(MABAgent[TSClippingParams]):
+    def __init__(self, params: TSClippingParams, environment: MABEnvironment) -> None:
+        super().__init__(params, environment)
+        self._posteriors: list[list[dist.Distribution]] = []
+        self._store_posteriors()
+
+    def __str__(self) -> str:
+        priors_text = str(
+            [likelihood.distribution_type for likelihood in self._params.likelihoods]
+        )
+        return f"TSClippingAgent(priors={priors_text})"
+
+    def __repr__(self) -> str:
+        return "TSClippingAgent"
+
+    def _store_posteriors(self) -> None:
+        posteriors = copy.deepcopy(
+            [likelihood.prior.distribution for likelihood in self._params.likelihoods]
+        )
+        self._posteriors.append(posteriors)
+
+    def observe(self, observation: MABObservation) -> None:
+        super().observe(observation)
+        self._params.likelihoods[observation.arm_pulled].update_prior(
+            [observation.reward]
+        )
+        self._store_posteriors()
+
+    def pick_action(self) -> int:
+        param_samples = [
+            likelihood.sample_prior_distribution()
+            for likelihood in self._params.likelihoods
+        ]
+        # torch distributions supoport broadcasting so a single distribution object can be used
+        beliefs: list[dist.Distribution] = [
+            likelihood.distribution(**params)
+            for likelihood, params in zip(self._params.likelihoods, param_samples)
+        ]
+        belief_samples = np.vstack(
+            [belief.sample(shape=(self._params.no_samples,)) for belief in beliefs]
+        )
+        best_arms = np.argmax(belief_samples, axis=0)
+        counts = np.bincount(best_arms)
+
+        probabilities = counts / counts.sum()
+        below_threshold = probabilities < self._params.clipping_threshold
+        probabilities[below_threshold] = self._params.clipping_threshold
+        mass_surplus = probabilities.sum() - 1
+        probabilities[~below_threshold] -= mass_surplus / (~below_threshold).sum()
+
+        return np.random.choice(np.arange(len(probabilities)), p=probabilities)
+
 
 @dataclass(frozen=True)
 class TSTopTwoParams(MABAgentParams):
@@ -63,18 +130,20 @@ class TSTopTwoParams(MABAgentParams):
 
     def __eq__(self, other) -> bool:
         return hash(self) == hash(other)
-    
+
 
 class TSTopTwoAgent(TSAgent):
     _params: TSTopTwoParams
 
     def __str__(self) -> str:
-        priors_text = str([likelihood.distribution_type for likelihood in self._params.likelihoods])
+        priors_text = str(
+            [likelihood.distribution_type for likelihood in self._params.likelihoods]
+        )
         return f"TSTopTwoAgent(priors={priors_text}, beta={self._params.beta})"
-    
+
     def __repr__(self) -> str:
         return "TSTopTwoAgent"
-    
+
     def pick_action(self) -> int:
         param_samples = [
             likelihood.sample_prior_distribution()
--- nsb/distributions/conjugacy.py
+++ nsb/distributions/conjugacy.py
@@ -15,12 +15,12 @@ class _DistributionLikeMixin:
     def __init__(
         self,
         distribution: t.Type[dist.Distribution],
-        **params: dict[str, float | PriorPosterior | tuple[t.Callable, PriorPosterior]]
+        **params: dict[str, float | PriorPosterior | tuple[t.Callable, PriorPosterior]],
     ) -> None:
-        
+
         for key, value in params.items():
             # Create the attribute with the key and value
-            setattr(self, f'_{key}', value)
+            setattr(self, f"_{key}", value)
         self._distribution = distribution
         self._distribution_params = tuple(params.keys())
         self._initial_param_values = tuple(params.values())
@@ -31,7 +31,7 @@ class _DistributionLikeMixin:
     @property
     def distribution_type(self) -> t.Type[dist.Distribution]:
         return self._distribution
-    
+
     def __hash__(self):
         params = [type(self)] + [
             (key, value)
@@ -39,10 +39,11 @@ class _DistributionLikeMixin:
             if isinstance(value, float)
         ]
         return utils.repeatable_hash(tuple(params))
-    
+
 
 class Likelihood(_DistributionLikeMixin, abc.ABC):
     """Base class for likelihoods"""
+
     @abc.abstractmethod
     def update_prior(self, data: list[float]) -> None:
         """Update the prior distribution"""
@@ -50,27 +51,29 @@ class Likelihood(_DistributionLikeMixin, abc.ABC):
     def distribution(self, **params: dict[str, float]) -> dist.Distribution:
         """Return the likelihood distribution"""
         return self._distribution(**params)
-    
-    def sample_prior_distribution(self) -> dict[str, float]:
+
+    def sample_prior_distribution(
+        self, no_samples: int | None = None
+    ) -> dict[str, float]:
         """Sample the distribution for params of the likelihood"""
         likelihood_params = {}
         for key in self._distribution_params:
-            value = getattr(self, f'_{key}')
+            value = getattr(self, f"_{key}")
             # The value is either a float, a PriorPosterior, or a (callable, PriorPosterior) tuple
             if isinstance(value, PriorPosterior):
-                value = value.sample_distribution()
+                value = value.sample_distribution(no_samples)
             elif isinstance(value, tuple):
                 func: t.Callable = value[0]
                 prior: PriorPosterior = value[1]
-                value = func(prior.sample_distribution())
+                value = func(prior.sample_distribution(no_samples))
             likelihood_params[key] = value
         return likelihood_params
-    
+
     @property
     def prior(self) -> PriorPosterior:
         # For now there's only one param with a prior on it
         for key in self._distribution_params:
-            value = getattr(self, f'_{key}')
+            value = getattr(self, f"_{key}")
             if isinstance(value, PriorPosterior):
                 return value
         raise ValueError(f"Couldn't find the prior for {self}!")
@@ -78,19 +81,22 @@ class Likelihood(_DistributionLikeMixin, abc.ABC):
 
 class PriorPosterior(_DistributionLikeMixin):
     """Base class for prior-posterior distributions"""
+
     @property
     def distribution(self) -> dist.Distribution:
         dist_params: dict[str, float] = {}
         for key in self._distribution_params:
-            value = getattr(self, f'_{key}')
+            value = getattr(self, f"_{key}")
             dist_params[key] = value
         dist = self._distribution(**dist_params)
         return dist
 
-    def sample_distribution(self) -> float:
+    def sample_distribution(self, no_samples: int | None = None) -> float:
         """Sample the distribution for params of the likelihood"""
+        if no_samples is not None:
+            return self.distribution.sample((no_samples,))
         return self.distribution.sample()
-    
+
     def __hash__(self) -> int:
         params = [type(self)] + [
             (key, value)
@@ -102,6 +108,7 @@ class PriorPosterior(_DistributionLikeMixin):
 
 class NormalPrior(PriorPosterior):
     _MIN_SCALE = 1e-32
+
     def __init__(self, loc: float, scale: float) -> None:
         super().__init__(distribution=dist.Normal, loc=loc, scale=scale)
 
@@ -110,17 +117,21 @@ class NormalPrior(PriorPosterior):
 class NormalKnownScaleLikelihood(Likelihood):
     def __init__(self, scale: float, loc: NormalPrior) -> None:
         super().__init__(distribution=dist.Normal, scale=scale, loc=loc)
-    
+
     def update_prior(self, data: list[float]) -> None:
         prior = self._loc
         mu_0 = prior._loc
-        sigma_0_sq = prior._scale ** 2
-        sigma_sq = self._scale ** 2
+        sigma_0_sq = prior._scale**2
+        sigma_sq = self._scale**2
         n = len(data)
         sum_xi = sum(data)
 
-        self._loc._loc = ( 1 / (1 / sigma_0_sq + n / sigma_sq) ) * (mu_0 / sigma_0_sq + sum_xi / sigma_sq)
-        self._loc._scale = max(1 / (1 / sigma_0_sq + n / sigma_sq), self._loc._MIN_SCALE)
+        self._loc._loc = (1 / (1 / sigma_0_sq + n / sigma_sq)) * (
+            mu_0 / sigma_0_sq + sum_xi / sigma_sq
+        )
+        self._loc._scale = max(
+            1 / (1 / sigma_0_sq + n / sigma_sq), self._loc._MIN_SCALE
+        )
 
 
 class InverseGammaPrior(PriorPosterior):
@@ -136,7 +147,7 @@ class NormalKnownLocLikelihood(Likelihood):
     def update_prior(self, data: list[float]) -> None:
         mu = self.loc
         n = len(data)
-        sum_xi_minus_mu_sq = sum((xi - mu)**2 for xi in data)
+        sum_xi_minus_mu_sq = sum((xi - mu) ** 2 for xi in data)
 
         self._variance._alpha = self._variance._alpha + n / 2
         self._variance._beta = self._variance._beta + sum_xi_minus_mu_sq / 2
@@ -144,7 +155,13 @@ class NormalKnownLocLikelihood(Likelihood):
 
 class NormalInverseGammaPrior(PriorPosterior):
     def __init__(self, mu: float, lambda_: float, alpha: float, beta: float) -> None:
-        super().__init__(distribution=NormalInverseGamma, mu=mu, lambda_=lambda_, alpha=alpha, beta=beta)
+        super().__init__(
+            distribution=NormalInverseGamma,
+            mu=mu,
+            lambda_=lambda_,
+            alpha=alpha,
+            beta=beta,
+        )
 
 
 # @utils.enforce_typehints
@@ -153,25 +170,19 @@ class NormalLikelihood(Likelihood):
     def __init__(self, loc_variance: NormalInverseGammaPrior) -> None:
         self._loc_variance = loc_variance
         super().__init__(
-            distribution=dist.Normal, 
+            distribution=dist.Normal,
             loc=(IthElement(0), loc_variance),
-            scale=(
-                FunctionComposition((math.sqrt, IthElement(1))),
-                loc_variance
-            )
+            scale=(FunctionComposition((math.sqrt, IthElement(1))), loc_variance),
         )
-        self._distribution_params = self._distribution_params + ('loc_variance',)  
-    
+        self._distribution_params = self._distribution_params + ("loc_variance",)
+
     @property
     def prior(self) -> NormalInverseGammaPrior:
         return self._loc_variance
-    
+
     def sample_prior_distribution(self) -> dict[str, float]:
         loc_variance = self._loc_variance.sample_distribution()
-        return {
-            'loc': loc_variance[0].item(),
-            'scale': loc_variance[1].item()
-        }
+        return {"loc": loc_variance[0].item(), "scale": loc_variance[1].item()}
 
     def update_prior(self, data: list[float]) -> None:
         mu_0 = self._loc_variance._mu
@@ -186,8 +197,9 @@ class NormalLikelihood(Likelihood):
         new_lambda = nu + n
         new_alpha = alpha + n / 2
         new_beta = (
-            beta + 0.5 * sum((xi - xbar)**2 for xi in data) +
-            (n * nu * (xbar - mu_0)**2) / (2 * (nu + n))
+            beta
+            + 0.5 * sum((xi - xbar) ** 2 for xi in data)
+            + (n * nu * (xbar - mu_0) ** 2) / (2 * (nu + n))
         )
         self._loc_variance._mu = new_mu
         self._loc_variance._lambda_ = new_lambda
